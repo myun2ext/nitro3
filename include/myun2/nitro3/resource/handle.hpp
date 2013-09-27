de0f7c626d19c816f42e04cbf28ab88e93771a9e
myun2 <myun2@nwhite.info>
2013-09-27 20:22:00 +0900

    resource/handle.hpp class Add.

---
+++ include/myun2/nitro3/resource/handle.hpp
@@ -0,0 +1,21 @@
+#ifndef __github_com_myun2__nitro3__resource__handle_HPP__
+#define __github_com_myun2__nitro3__resource__handle_HPP__
+
+namespace myun2
+{
+	namespace nitro3
+	{
+		namespace resource
+		{
+			template <typename T> class handle
+			{
+			private:
+				T _handle;
+			public:
+				
+			};
+		}
+	}
+}
+
+#endif//__github_com_myun2__nitro3__resource__handle_HPP__
