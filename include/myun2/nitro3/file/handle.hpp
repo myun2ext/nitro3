6a8222ac7ef9704ba658eeddfcd78c2d8d25811a
myun2 <myun2@nwhite.info>
2013-09-27 20:18:16 +0900

    file/handle.hpp class Add

---
+++ include/myun2/nitro3/file/handle.hpp
@@ -0,0 +1,17 @@
+#ifndef __github_com_myun2__nitro3__file__handle_HPP__
+#define __github_com_myun2__nitro3__file__handle_HPP__
+
+namespace myun2
+{
+	namespace nitro3
+	{
+		namespace file
+		{
+			class handle
+			{
+			};
+		}
+	}
+}
+
+#endif//__github_com_myun2__nitro3__file__handle_HPP__
