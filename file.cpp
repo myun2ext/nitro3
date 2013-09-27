07eb2fa2cb1045c050d67fcd7affdc46e1516098
myun2 <myun2@nwhite.info>
2013-09-27 23:10:25 +0900

    closer

--- file.cpp
+++ file.cpp
@@ -2,7 +2,12 @@
 #include "myun2/nitro3/resource/handle.hpp"
 
 using namespace myun2::nitro3::resource;
+
+void closer(FILE* fp) { fclose(fp); }
+
 int main()
 {
+	handle<FILE*, closer> h;
+
 	return 0;
 }
