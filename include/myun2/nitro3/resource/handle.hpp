8765cd3efdf8ea035ce4ab870972d5a118420d12
myun2 <myun2@nwhite.info>
2013-09-27 23:03:58 +0900

    holder_clone();

--- include/myun2/nitro3/resource/handle.hpp
+++ include/myun2/nitro3/resource/handle.hpp
@@ -22,10 +22,13 @@ namespace myun2
 					static *holder allocate() { return new T; }
 					static *holder allocate(const T& in_handle) { return new T(in_handle); }
 				};
-				holder *p_holder;
+				mutable holder *p_holder;
+				void increment_reference() const { p_holder->referenced_count++; }
+				holder* holder_clone() const { increment_reference(); return p_holder; }
 			public:
 				handle(){ p_holder = 0; }
 				handle(const T& in_handle) { p_holder = holder::allocate(in_handle); }
+				handle(const handle& in_handle) { p_holder = handle.holder_clone(); }
 				virtual ~handle() {
 					if ( p_holder != 0 ) _Releaser(in_handle);
 				}
