a94e2a8de417faf5848654807d5bec3ce9c3d09c
myun2 <myun2@nwhite.info>
2013-09-27 19:08:54 +0900

    nitrogen/injector.hpp Add

---
+++ include/myun2/nitro3/nitrogen/injector.hpp
@@ -0,0 +1,17 @@
+#ifndef __github_com_myun2__nitro__nitrogen__injector_HPP__
+#define __github_com_myun2__nitro__nitrogen__nitrogen_HPP__
+
+namespace myun2
+{
+	namespace nitro3
+	{
+		namespace nitrogen
+		{
+			class injector
+			{
+			};
+		}
+	}
+}
+
+#endif//__github_com_myun2__nitro__nitrogen__injector_HPP__
--- include/myun2/nitro3/nitrogen/nitrogen.hpp
+++ include/myun2/nitro3/nitrogen/nitrogen.hpp
@@ -1,14 +1,6 @@
 #ifndef __github_com_myun2__nitro__nitrogen__nitrogen_HPP__
 #define __github_com_myun2__nitro__nitrogen__nitrogen_HPP__
 
-namespace myun2
-{
-	namespace nitro3
-	{
-		namespace nitrogen
-		{
-		}
-	}
-}
+#include "myun2/nitro3/nitrogen/injector.hpp"
 
 #endif//__github_com_myun2__nitro__nitrogen__nitrogen_HPP__
