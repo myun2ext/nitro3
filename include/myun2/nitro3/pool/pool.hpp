e1b43716f359983da488e44cfc19b21768c4e264
myun2 <myun2@nwhite.info>
2013-10-01 17:03:24 +0900

    pool.hpp Add

---
+++ include/myun2/nitro3/pool/pool.hpp
@@ -0,0 +1,14 @@
+#ifndef __github_com_myun2__nitro__pool_HPP__
+#define __github_com_myun2__nitro__pool_HPP__
+
+namespace myun2
+{
+	namespace nitro3
+	{
+		namespace pool
+		{
+		}
+	}
+}
+
+#endif//__github_com_myun2__nitro__pool_HPP__
