42826b867806597a1c96da76f545cabdab6792b0
myun2 <myun2@nwhite.info>
2013-10-01 17:10:36 +0900

    memory_pool.hpp and file_pool.hpp

---
+++ include/myun2/nitro3/pool/file_pool.hpp
@@ -0,0 +1,15 @@
+#ifndef __github_com_myun2__nitro__file_pool_HPP__
+#define __github_com_myun2__nitro__file_pool_HPP__
+
+namespace myun2
+{
+	namespace nitro3
+	{
+		namespace pool
+		{
+			class file_pool;
+		}
+	}
+}
+
+#endif//__github_com_myun2__nitro__file_pool_HPP__
