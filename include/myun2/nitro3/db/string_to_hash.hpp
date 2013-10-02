b4124d7f62dc1901e3348b24dc08ed716cafa3d0
myun2 <myun2@nwhite.info>
2013-10-02 15:39:49 +0900

    db/string_to_hash.hpp Add

---
+++ include/myun2/nitro3/db/string_to_hash.hpp
@@ -0,0 +1,18 @@
+#ifndef __github_com_myun2__nitro__db__string_to_hash_HPP__
+#define __github_com_myun2__nitro__db__string_to_hash_HPP__
+
+namespace myun2
+{
+	namespace nitro3
+	{
+		namespace db
+		{
+			template <unsigned int hash_bits, typename T=unsigned int>
+			T string_to_hash(const void* s, unsigned int length)
+			{
+			}
+		}
+	}
+}
+
+#endif//__github_com_myun2__nitro__db__string_to_hash_HPP__
