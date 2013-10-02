df052f9453d6c58bb44e8bf30be2240ac7b0b8ed
myun2 <myun2@nwhite.info>
2013-10-02 18:05:34 +0900

    page.hpp fix

--- include/myun2/nitro3/db/page.hpp
+++ include/myun2/nitro3/db/page.hpp
@@ -1,6 +1,8 @@
 #ifndef __github_com_myun2__nitro__db__page_HPP__
 #define __github_com_myun2__nitro__db__page_HPP__
 
+#include <string.h>
+
 namespace myun2
 {
 	namespace nitro3
@@ -21,18 +23,18 @@ namespace myun2
 					return *key;
 				}
 			public:
-				page(_IndexFile& index, unsigned int page_head_in) : _index(index), page_head(page_head_in) {}
+				page(_IndexFile& index, unsigned int page_head_in=0) : _index(index), page_head(page_head_in) {}
 
 				///////////////////////
 
-				index_t write(const char* key, const &_Value v) { return write(key, strlen(key), v); }
-				index_t write(const void* key, unsigned int key_length, const &_Value v) {
-					return _impl.write(page_head_in + key_to_index(key, key_length), v);
+				index_t write(const char* key, const _Value& v) { return write(key, strlen(key), v); }
+				index_t write(const void* key, unsigned int key_length, const _Value& v) {
+					return _index.write(page_head + key_to_index(key, key_length), v);
 				}
 
 				_Value read(const char* key) { return read(key, strlen(key)); }
 				_Value read(const void* key, unsigned int key_length) {
-					return _impl.read(page_head_in + key_to_index(key, key_length));
+					return _index.read(page_head + key_to_index(key, key_length));
 				}
 			};
 		}
