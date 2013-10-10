475503771f8144b5b52ff94baaa3a0597bb933f1
myun2 <myun2@nwhite.info>
2013-10-10 11:52:57 +0900

    entries

--- include/myun2/nitro3/db/btree_index.hpp
+++ include/myun2/nitro3/db/btree_index.hpp
@@ -9,14 +9,16 @@ namespace myun2
 	{
 		namespace db
 		{
-			template <typename _Impl, unsigned int _PageEntries>
+			template <typename _Impl, unsigned int _PageEntries=256>
 			class btree_index
 			{
 			public:
+				typedef _Impl::index_t index_t;
 			private:
 				_Impl& file;
 				struct page
 				{
+					index_t entries[_PageEntries];
 				};
 			public:
 				btree_index(_Impl& _file) : file(_file) {}
