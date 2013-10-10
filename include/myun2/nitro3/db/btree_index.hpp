76f607cf2688a572d894ab76544c36613db61f4f
myun2 <myun2@nwhite.info>
2013-10-10 11:48:52 +0900

    small, minimum btree_index.hpp

--- include/myun2/nitro3/db/btree_index.hpp
+++ include/myun2/nitro3/db/btree_index.hpp
@@ -9,86 +9,17 @@ namespace myun2
 	{
 		namespace db
 		{
-			template <typename _Impl, unsigned int _PageSize=256, typename _KeyType=unsigned int, typename _IndexType=unsigned char>
+			template <typename _Impl, unsigned int _PageEntries>
 			class btree_index
 			{
 			public:
-				//typedef typename _Impl::index_t index_t;
-				typedef size_t pos_t;
-				typedef _IndexType index_t;
-				typedef _KeyType key_t;
 			private:
-				static const size_t head = sizeof(_IndexType);
-				static const _KeyType null_key = 0;
-				static const size_t null_idx = 0;
-
 				_Impl& file;
-				struct Entry
+				struct page
 				{
-					index_t i;
-					index_t prev, next;
-					key_t key;
-
-					//Entry() : i(null_idx), key(null_key), prev(null_idx), next(null_idx) {}
-					Entry(){}
-					Entry(index_t i_, key_t key_) : i(i_), key(key_), prev(null_idx), next(null_idx) {}
-
-					bool is_none() const { return i == null_idx; }
-					bool is_key_none() const { return key == null_key; }
 				};
-				struct EntryResult
-				{
-					Entry& e;
-					pos_t pos;
-					EntryResult(Entry& e_, pos_t pos_) : e(e_), pos(pos_) {}
-				};
-
-				Entry page[_PageSize];
-				void read_page() {
-					memset(page, 0, sizeof(page));
-					file._read(0, page, sizeof(page));
-				}
-				void write_page() {
-					file._write(0, page, sizeof(page));
-				}
-
-				const Entry& read(pos_t pos) const { return page[pos]; }
-				Entry& read(pos_t pos) { return page[pos]; }
-				void write(pos_t pos, const Entry &entry) { page[pos] = entry; }
-public:
-				//index_t find_entry(pos_t pos, const _KeyType &key) {
-				EntryResult find_entry(const _KeyType &key, pos_t pos=head) {
-					Entry& e = read(pos);
-					if ( e.i == null_idx )
-						return EntryResult(e, pos);
-					if ( e.key == key )
-						return EntryResult(e, pos);
-					if ( key < e.key )
-						return find_entry(key, e.prev);
-					else
-						return find_entry(key, e.next);
-				}
 			public:
-				btree_index(_Impl& _file) : file(_file) {
-					read_page();
-				}
-
-				index_t find(const _KeyType &key) {
-					return find_entry(key).i;
-				}
-
-				index_t append(const _KeyType &key) {
-					Entry& e = find_entry(key);
-					if ( e.is_none() ) {
-						e.key = key;
-						save();
-						
-					}
-					//file.write(key_to_index(key));
-				}
-
-				void save() { write_page(); }
-				void flush() { write_page(); }
+				btree_index(_Impl& _file) : file(_file) {}
 			};
 		}
 	}
