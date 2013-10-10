69314417caf1e863cd56f057e7d0f3ae5db1f99c
myun2 <myun2@nwhite.info>
2013-10-10 19:07:18 +0900

    btree: page entry::add(), read_page() and write_page()

--- include/myun2/nitro3/db/btree_index.hpp
+++ include/myun2/nitro3/db/btree_index.hpp
@@ -9,35 +9,50 @@ namespace myun2
 	{
 		namespace db
 		{
-			template <typename _Impl, typename _KeyType=unsigned int, unsigned int _PageEntries=256>
+			template <typename _Impl, typename _ValueType=unsigned int, typename _KeyType=unsigned int, unsigned int _PageEntries=256>
 			class btree_index
 			{
 			public:
 				typedef typename _Impl::index_t index_t;
+				typedef _ValueType value_t;
 				typedef _KeyType key_t;
 			private:
 				_Impl& file;
 				struct page
 				{
-					typedef typename _Impl::index_t entry_t;
+					//typedef typename _Impl::index_t entry;
+					struct Entry
+					{
+						key_t key;
+						value_t value;
+					};
+					//typedef typename _Impl::index_t Entry;
 					typedef size_t index_t;
 					static const unsigned int max = _PageEntries;
 					static const unsigned int entry_max = _PageEntries;
 					static const index_t null_idx = 0;
 
-					entry_t entries[entry_max];
+					Entry entries[entry_max];
 
-					entry_t& at(const index_t& i) { return entries[i]; }
-					const entry_t& at(const index_t& i) const { return entries[i]; }
+					Entry& at(const index_t& i) { return entries[i]; }
+					const Entry& at(const index_t& i) const { return entries[i]; }
 
-					entry_t& find_first() {}
-					entry_t& find_first_empty() {
+					Entry& find_first() {}
+					Entry& find_first_empty() {
 						for(size_t i=0; i<max; i++) {
 							if ( at(i) == null_idx )
 								return at(i);
 						}
 					}
+
+					Entry& add(page& p, const value_t& v) {
+						Entry& e = find_first_empty();
+						e.value = v;
+						return e;
+					}
 				};
+				static const size_t page_size = sizeof(page);
+
 				void init_page(page& p) {
 					memset(&p, 0, sizeof(page));
 				}
@@ -45,6 +60,17 @@ namespace myun2
 				void add(page& p, const T& v) {
 					
 				}
+
+				size_t page_pos(unsigned int page_num) const { return page_num * page_size; }
+				page& read_page(page& p, unsigned int page_num) {
+					file._read(page_pos(page_num), &p, sizeof(page));
+					return p;
+				}
+				void write_page() {
+					file._write(page_pos(page_num), &p, sizeof(page));
+					return p;
+				}
+
 			public:
 				btree_index(_Impl& _file) : file(_file) {}
 
