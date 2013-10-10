e7b33e12a11a946342231b3a2b5c4bf21cf8f076
myun2 <myun2@nwhite.info>
2013-10-10 18:32:14 +0900

    btree: page -> find_first_empty() implement

--- include/myun2/nitro3/db/btree_index.hpp
+++ include/myun2/nitro3/db/btree_index.hpp
@@ -9,22 +9,51 @@ namespace myun2
 	{
 		namespace db
 		{
-			template <typename _Impl, unsigned int _PageEntries=256>
+			template <typename _Impl, typename _KeyType=unsigned int, unsigned int _PageEntries=256>
 			class btree_index
 			{
 			public:
-				typedef _Impl::index_t index_t;
+				typedef typename _Impl::index_t index_t;
+				typedef _KeyType key_t;
 			private:
 				_Impl& file;
 				struct page
 				{
-					index_t entries[_PageEntries];
+					typedef typename _Impl::index_t entry_t;
+					typedef size_t index_t;
+					static const unsigned int max = _PageEntries;
+					static const unsigned int entry_max = _PageEntries;
+					static const index_t null_idx = 0;
+
+					entry_t entries[entry_max];
+
+					entry_t& at(const index_t& i) { return entries[i]; }
+					const entry_t& at(const index_t& i) const { return entries[i]; }
+
+					entry_t& find_first() {}
+					entry_t& find_first_empty() {
+						for(size_t i=0; i<max; i++) {
+							if ( at(i) == null_idx )
+								return at(i);
+						}
+					}
 				};
 				void init_page(page& p) {
 					memset(&p, 0, sizeof(page));
 				}
+				template <typename T>
+				void add(page& p, const T& v) {
+					
+				}
 			public:
 				btree_index(_Impl& _file) : file(_file) {}
+
+				void append(const key_t& k) {
+					
+				}
+
+				void flush() {}
+				void save() {}
 			};
 		}
 	}
