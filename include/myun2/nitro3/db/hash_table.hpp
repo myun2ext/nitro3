605c1674181a83a4e94a591de6f4cc17527e50c1
myun2 <myun2@nwhite.info>
2013-10-11 20:24:40 +0900

    hash_table.hpp updated

--- include/myun2/nitro3/db/hash_table.hpp
+++ include/myun2/nitro3/db/hash_table.hpp
@@ -9,15 +9,23 @@ namespace myun2
 	{
 		namespace db
 		{
-			template <typename _Impl, unsigned int _HashTableLength=256>
+			template <typename _Impl, typename _HashKey, typename _HashValue, unsigned int _HashTableLength=256>
 			class hash_table
 			{
 			public:
 				typedef typename _Impl::index_t index_t;
+				typedef _HashKey key_t;
+				typedef _HashValue value_t;
 			private:
 				_Impl& file;
 
-				index_t hash_table[_HashTableLength];
+				struct entry
+				{
+					key_t key;
+					value_t v;
+				};
+				entry entries[_HashTableLength];
+
 				void read_page() {
 					file._read(0, page, sizeof(page));
 				}
