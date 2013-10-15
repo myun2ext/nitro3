0abf5c79b7ba0b33b6916e4a7bd76944108d09af
myun2 <myun2@nwhite.info>
2013-10-15 14:17:43 +0900

    shape up

--- include/myun2/nitro3/db/string_pool.hpp
+++ include/myun2/nitro3/db/string_pool.hpp
@@ -9,51 +9,15 @@ namespace myun2
 	{
 		namespace db
 		{
-			template <typename _Impl, typename _HashKey=unsigned int, typename _HashValue=unsigned int, unsigned int _HashTableLength=2048>
+			template <typename _Impl>
 			class string_pool
 			{
 			public:
 				typedef typename _Impl::index_t index_t;
-				typedef _HashKey key_t;
-				typedef _HashValue value_t;
 			private:
 				_Impl& file;
-
-				struct entry
-				{
-					key_t key;
-					value_t value;
-				};
-				entry entries[_HashTableLength];
-				size_t chop(const key_t& hash) const {
-					return hash % _HashTableLength;
-				}
-				entry& at(size_t i) {
-					return entries[i];
-				}
-				const entry& at(size_t i) const {
-					return entries[i];
-				}
 			public:
-				string_pool(_Impl& _file) : file(_file) { reload(); }
-
-				value_t& operator[] (const key_t& hash) {
-					size_t chopped = chop(hash);
-					return at(chopped).value;
-				}
-				const value_t& operator[] (const key_t& hash) const {
-					size_t chopped = chop(hash);
-					return at(chopped).value;
-				}
-
-				void reload() {
-					memset(entries, 0, sizeof(entries));
-					file._read(0, entries, sizeof(entries));
-				}
-				void flush() {
-					file._write(0, entries, sizeof(entries));
-				}
-				void save(){ flush(); }
+				string_pool(_Impl& _file) : file(_file) {}
 			};
 		}
 	}
