961b398c4f7bb9ceda09183b4a1acbdf82a3b86e
myun2 <myun2@nwhite.info>
2013-10-17 12:20:38 +0900

    db/simple/simple_db.hpp Add

---
+++ include/myun2/nitro3/db/simple/simple_db.hpp
@@ -0,0 +1,17 @@
+#ifndef __github_com_myun2__nitro__db__simple__simple_db_HPP__
+#define __github_com_myun2__nitro__db__simple__simple_db_HPP__
+
+namespace myun2
+{
+	namespace nitro3
+	{
+		namespace db
+		{
+			class simple_db
+			{
+			};
+		}
+	}
+}
+
+#endif//__github_com_myun2__nitro__db__simple__simple_db_HPP__
