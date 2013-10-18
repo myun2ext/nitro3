eb1a9db204cca3fc31a993bb9666668728864b5b
myun2 <myun2@nwhite.info>
2013-10-18 16:31:05 +0900

    simple/simple_db.hpp updated

--- include/myun2/nitro3/db/simple/simple_db.hpp
+++ include/myun2/nitro3/db/simple/simple_db.hpp
@@ -1,15 +1,19 @@
 #ifndef __github_com_myun2__nitro__db__simple__simple_db_HPP__
 #define __github_com_myun2__nitro__db__simple__simple_db_HPP__
 
+#include "myun2/nitro3/pool/file_pool.hpp"
+
 namespace myun2
 {
 	namespace nitro3
 	{
 		namespace db
 		{
-			class simple_db
+			template <typename _Impl = myun2::nitro3::pool::file_pool>
+			class simple_db_
 			{
 			};
+			typedef simple_db_<> simple_db;
 		}
 	}
 }
